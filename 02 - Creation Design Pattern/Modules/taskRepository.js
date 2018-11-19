c2d9e75c23dede3048b0e3bde10627edab3f0083
Mike Bova <mbova407@yahoo.com>
2018-11-19 15:17:00 -0500

    Revealing Pattern

--- 02 - Creation Design Pattern/Modules/taskRepository.js
+++ 02 - Creation Design Pattern/Modules/taskRepository.js
@@ -1,14 +1,17 @@
 	var repo = function () {
-		return {
-			get: function (id) {
-				console.log('Getting task ' + id);
-				return {
-					name: 'new task from db'
-				}
-			},
-			save: function (task) {
-				console.log('Saving ' + task.name + ' to the db');
+		var get = function (id) {
+			console.log('Getting task ' + id);
+			return {
+				name: 'new task from db'
 			}
+		};
+		var save= function (task) {
+			console.log('Saving ' + task.name + ' to the db');
+		};
+		
+		return {
+			get:get,
+			save: save
 		}
 	}
 
